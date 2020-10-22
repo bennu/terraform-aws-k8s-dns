12c0a90d1c0a7e628795a14f5e04ff9edae8a092
Pierre Ugaz <ulm0@innersea.xyz>
2020-10-22 18:39:20 -0300

    chore: no longer create dex record

--- output.tf
+++ output.tf
@@ -1,5 +1,4 @@
 output api_server_fqdn { value = aws_route53_record.record.0.fqdn }
-output dex_fqdn { value = aws_route53_record.record.1.fqdn }
 output name_servers { value = aws_route53_zone.zone.name_servers }
 output record { value = local.records }
 output zone_id { value = aws_route53_zone.zone.zone_id }
